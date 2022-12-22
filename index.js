c59d8f5ed9f82572bedd1e2443f150f5fdf6b01f
Amir Sabagh <amir.sabagh80@gmail.com>
2022-12-22 11:27:43 -0500

    Handle clicks on like button

--- index.js
+++ index.js
@@ -6,42 +6,57 @@ tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
 })
 
-function getFeedHtml(){
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+       handleLikeClick(e.target.dataset.like) 
+    }
+})
+
+function handleLikeClick(tweetId){ 
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
     
+    targetTweetObj.likes++
+    
+    render()
+}
+
+function getFeedHtml(){
     let feedHtml = ``
     
     tweetsData.forEach(function(tweet){
         feedHtml += `
-        <div class="tweet">
-            <div class="tweet-inner">
-                <img src="${tweet.profilePic}" class="profile-pic">
-                <div>
-                    <p class="handle">${tweet.handle}</p>
-                    <p class="tweet-text">${tweet.tweetText}</p>
-                    <div class="tweet-details">
-                        <span class="tweet-detail">
-                            <i class="fa-regular fa-comment-dots"
-                            data-reply="${tweet.uuid}"
-                            ></i>
-                            ${tweet.replies.length}
-                        </span>
-                        <span class="tweet-detail">
-                            <i class="fa-solid fa-heart"
-                            data-like="${tweet.uuid}"
-                            ></i>
-                            ${tweet.likes}
-                        </span>
-                        <span class="tweet-detail">
-                            <i class="fa-solid fa-retweet"
-                            data-retweet="${tweet.uuid}"
-                            ></i>                       
-                            ${tweet.retweets}
-                        </span>
-                    </div>   
-                </div>            
-            </div>
-        </div>
-        `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart"
+                    data-like="${tweet.uuid}"
+                    ></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`
    })
    return feedHtml 
 }
@@ -49,5 +64,6 @@ function getFeedHtml(){
 function render(){
     document.getElementById('feed').innerHTML = getFeedHtml()
 }
-render();
+
+render()
 
