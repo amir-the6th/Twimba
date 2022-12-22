4d721c08a359c09c58f15e9806d0773be405e4ae
Amir Sabagh <amir.sabagh80@gmail.com>
2022-12-22 12:30:53 -0500

    Move a declaration to a local scope

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { tweetsData } from './data.js';
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
-const tweetInput = document.getElementById('tweet-input')
-
 document.addEventListener('click', function(e){
     if(e.target.dataset.like){
        handleLikeClick(e.target.dataset.like) 
@@ -53,6 +51,8 @@ function handleReplyClick(replyId){
 }
 
 function handleTweetBtnClick(){
+    const tweetInput = document.getElementById('tweet-input')
+
     if(tweetInput.value) {
         tweetsData.unshift({
         handle: `@Scrimba`,
