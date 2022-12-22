7e41d940ad31a38c23db7a5a8b7e6f5ac7fc169e
Amir Sabagh <amir.sabagh80@gmail.com>
2022-12-22 11:04:55 -0500

    Add reply, like, retweet icons and set data attributes

--- index.js
+++ index.js
@@ -20,12 +20,21 @@ function getFeedHtml(){
                     <p class="tweet-text">${tweet.tweetText}</p>
                     <div class="tweet-details">
                         <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots"
+                            data-reply="${tweet.uuid}"
+                            ></i>
                             ${tweet.replies.length}
                         </span>
                         <span class="tweet-detail">
+                            <i class="fa-solid fa-heart"
+                            data-like="${tweet.uuid}"
+                            ></i>
                             ${tweet.likes}
                         </span>
                         <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet"
+                            data-retweet="${tweet.uuid}"
+                            ></i>                       
                             ${tweet.retweets}
                         </span>
                     </div>   
