ac28074fcb827a6d6b09e3f104aa737af8e4b2a5
Amir Sabagh <amir.sabagh80@gmail.com>
2022-12-22 10:23:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Twimba-Twitter-Clone
+ Twimba is a simple clone of the Twitter interface, containing a number of tweets and working functionalities such as like, retweet, and comment
---
+++ index.js
@@ -0,0 +1,49 @@
+import { tweetsData } from './data.js'
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value)
+})
+
+function getFeedHtml(){
+    
+    let feedHtml = ``
+    
+    tweetsData.forEach(function(tweet){
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`
+   })
+   return feedHtml 
+}
+
+function render(){
+/*
+Challenge:
+1. Take control of the ‘feed’ div.
+2. Render the HTML returned by the getFeedHtml 
+   function to the 'feed' div. 
+   See if you can do this with just one line of code!
+*/
+}
+
